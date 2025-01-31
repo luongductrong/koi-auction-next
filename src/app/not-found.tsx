d2619322c427875a92f51fd89327e71f7fba9c67
luongductrong <luongductrong2004@gmail.com>
2025-02-01 00:34:14 +0700

    Update: NotFound page: emove dependency on Ant Design; TailwindCSS: use custom theme variable

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,29 +1,17 @@
-import { memo } from 'react';
 import type { Metadata } from 'next';
 import Link from 'next/link';
-import { Button, ConfigProvider } from 'antd';
 
-export default memo(function NotFound() {
+export default function NotFound() {
   return (
-    <ConfigProvider
-      theme={{
-        components: {
-          Button: {
-            borderRadius: 20,
-          },
-        },
-      }}
-    >
-      <div className="flex flex-col items-center justify-center text-center my-20 mx-auto">
-        <h1 className="text-6xl font-bold m-0 ">404</h1>
-        <p className="text-2xl my-4">Không tìm thấy trang bạn yêu cầu</p>
-        <Link href="/">
-          <Button type="primary">Về trang chủ</Button>
-        </Link>
-      </div>
-    </ConfigProvider>
+    <div className="flex flex-col items-center justify-center text-center my-20 mx-auto">
+      <h1 className="text-6xl text-primary font-bold m-0">404</h1>
+      <p className="text-2xl my-4">Không tìm thấy trang bạn yêu cầu</p>
+      <Link href="/" className="rounded-[20px] bg-primary text-white text-sm hover:bg-primary/90 leading-8 px-4">
+        Về trang chủ
+      </Link>
+    </div>
   );
-});
+}
 
 export const metadata: Metadata = {
   title: '404 - Không tìm thấy trang | Koi Auction',
